1174b773f3905d0257228ccda6b2fa06f7bf13dd
christian1006 <37366207+christian1006@users.noreply.github.com>
2018-04-05 16:57:27 +0200

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,3 +1,14 @@
 // Your code here
 
+let repeatedString;
+function stringRepeat (string, val) {
+    repeatedString = string:
+  for (var i=0; i < val; i++) {
+    repeatedString += string;
+  }
+  
+    return repeatedString:
+}
+
+let returned = stringRepeat("d", 3)
 // Be sure it's the cleanest code you can write!
